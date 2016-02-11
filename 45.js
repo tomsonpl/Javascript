14ff774a6ad426fa9489feef4a0860d30fef1dc2
tomsonpl <ciecierskitomek@gmail.com>
2016-02-11 21:47:51 +0100

    JS Zadania Praktyczne
    Zaczynam od 4.4, mam zamiar przerobić wszystkie.

---
+++ 45.js
@@ -0,0 +1,8 @@
+var liczba = 124;
+function func() {
+	document.write ("wartość zmiennej w funkcji: " + liczba);
+}
+
+func();
+document.write ("<br />");
+document.write ("wartość zmiennej poza funkcją: " + liczba);
